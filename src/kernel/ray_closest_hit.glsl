1ff16a2a69dc95421ba437564df2bccab6290cf4
anguslang <lilang8936@gmail.com>
2020-07-17 20:54:28 +0800

    add sample AO

--- src/kernel/ray_closest_hit.glsl
+++ src/kernel/ray_closest_hit.glsl
@@ -1,4 +1,4 @@
-// #define ENV_SAMPLE
+// #define SAMPLE_AO
 
 /**
  * global variable
@@ -7,7 +7,17 @@
  **/
 ray ray_closest_hit(const ray ray_input, const trace_result result, const material mat) {
 
-#ifdef ENV_SAMPLE
+#ifdef SAMPLE_AO
+  // Simple AO Sampling
+  vec3 reflect_direction = hammersley_sample_cos(result.normal, frame_index, sample_count);
+  ray bounce_ray = ray(result.position + reflect_direction * EPSILON, reflect_direction);
+  bool blocked = trace_shadow(bounce_ray);
+  color += vec4(mix(vec3(1.0), vec3(0.0), float(blocked)), 1.0);
+
+  terminated = true;
+  return ray_input;
+
+#elif defined(SAMPLE_ENV)
   // Simple Env Sampling
   vec3 reflect_direction = hammersley_sample_cos(result.normal, frame_index, sample_count);
   ray bounce_ray = ray(result.position + reflect_direction * EPSILON, reflect_direction);
